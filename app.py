45b8065f190f95b761021dc122dff36523daf3d8
Kolade <kolade.adenuga@gmail.com>
2022-02-12 14:08:17 +0100

    hopes

---
+++ app.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+import uvicorn
+import pickle
+from pydantic import BaseModel
+
+class BankNote(BaseModel):
+    variance : float
+    skewness: float
+    curtosis: float
+    entropy: float
+
+
+app = FastAPI(debug=True)
+
+classifier = pickle.load(open('hopes.pickle','rb'))
+status =''
+
+
+@app.get('/')
+def home():
+    return {'text':'This is a trial'} 
+
+@app.post('/predict')
+def predict_notes(data: BankNote):
+    data = data.dict()
+    variance = data['vaiance']
+    skewness = data['skewness']
+    curtosis = data['curtosis']
+    entropy = data['entropy']
+
+    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
+
+    if(prediction[0]>0.5):
+        status = 'FakeNote'
+    else:
+        status = 'Original'
+    return {
+        'prediction': status
+    }
+
+if __name__ == '__main__':
+    uvicorn.run(app)
